d0b6338f3cf9d8a2be7db5f36d06c3aa9d169ae7
sthompson-athenian <sthompson@athenian.org>
2025-08-22 16:02:14 -0700

    Update Gradle to 7.6, Java to 17, and apply code formatting

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,28 +1,39 @@
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
 
 public class MyTests {
-    @Test
-    public void testAdd() {
-        GroceryList list1 = new GroceryList();
-        list1.add("milk");
-        assertEquals("Grocery List: milk", list1.toString().trim().replaceFirst(",$", ""), "Adding milk to an empty list should give: \"Grocery List: milk\"");
-        list1.add("eggs");
-        list1.add("cereal");
-        assertEquals("Grocery List: milk, eggs, cereal", list1.toString().trim().replaceFirst(",$", ""), "Adding eggs and cereal should give: \"Grocery List: milk\"");
-    }
+  @Test
+  public void testAdd() {
+    GroceryList list1 = new GroceryList();
+    list1.add("milk");
+    assertEquals(
+        "Grocery List: milk",
+        list1.toString().trim().replaceFirst(",$", ""),
+        "Adding milk to an empty list should give: \"Grocery List: milk\"");
+    list1.add("eggs");
+    list1.add("cereal");
+    assertEquals(
+        "Grocery List: milk, eggs, cereal",
+        list1.toString().trim().replaceFirst(",$", ""),
+        "Adding eggs and cereal should give: \"Grocery List: milk\"");
+  }
 
-    @Test
-    public void testRemove() {
-        GroceryList list1 = new GroceryList();
-        list1.add("milk");
-        list1.add("eggs");
-        list1.add("cereal");
-        list1.remove("milk");
-        assertEquals("Grocery List: eggs, cereal", list1.toString().trim().replaceFirst(",$", ""), "Removing milk should give: \"Grocery List: eggs, cereal\"");
-        list1.remove("cereal");
-        assertEquals("Grocery List: eggs", list1.toString().trim().replaceFirst(",$", ""), "Removing cereal should give: \"Grocery List: eggs\"");
-    }
-    
+  @Test
+  public void testRemove() {
+    GroceryList list1 = new GroceryList();
+    list1.add("milk");
+    list1.add("eggs");
+    list1.add("cereal");
+    list1.remove("milk");
+    assertEquals(
+        "Grocery List: eggs, cereal",
+        list1.toString().trim().replaceFirst(",$", ""),
+        "Removing milk should give: \"Grocery List: eggs, cereal\"");
+    list1.remove("cereal");
+    assertEquals(
+        "Grocery List: eggs",
+        list1.toString().trim().replaceFirst(",$", ""),
+        "Removing cereal should give: \"Grocery List: eggs\"");
+  }
 }
